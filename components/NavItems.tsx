1313bb98dd6f7e7f7c0efd44505fcea77da76ac0
PiyushYadavPY <piyushyadav4991@gmail.com>
2026-01-31 15:12:01 +0530

    Typo Fixes

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -10,7 +10,7 @@ const NavItems = () => {
         return pathName.startsWith(path)
     }
   return (
-    <ul className='flex flex-col sm:flex-row p-2  gap-3 s,:gap-10 font-medium'>
+    <ul className='flex flex-col sm:flex-row p-2  gap-3 sm:gap-10 font-medium'>
             {NAV_ITEMS.map(({href, label}) => (
                 <li>
                 <Link href={href} className={`hover:text-yellow-500 transition-colors ${
